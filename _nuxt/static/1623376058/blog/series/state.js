12bc556dd6d472bc1ea2b31cb2938ffadbf54caf
thombruce <thombruce@users.noreply.github.com>
2021-06-11 01:47:48 +0000

    deploy: 574197ebea9ffba964ce6563f271b1d3de195966

---
+++ _nuxt/static/1623376058/blog/series/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623376058",layout:"default",error:a,serverRendered:b,routePath:"\u002Fblog\u002Fseries",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bef26b82"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
