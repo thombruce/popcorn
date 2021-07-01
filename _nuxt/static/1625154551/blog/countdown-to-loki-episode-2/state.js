12d5ab8bc0e79b5605f8ba12ea1c073efdb9f8ee
thombruce <thombruce@users.noreply.github.com>
2021-07-01 15:49:29 +0000

    deploy: 52e13409cc43f7a566e02b24121a7885368d2156

---
+++ _nuxt/static/1625154551/blog/countdown-to-loki-episode-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625154551",layout:"default",error:a,serverRendered:false,routePath:"\u002Fblog\u002Fcountdown-to-loki-episode-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cc6923c5"}}}}(null));
