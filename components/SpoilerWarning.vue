76448f48b9d83057d683f364541fbfa5e9c6ce23
Thom Bruce <thom@thombruce.com>
2021-07-30 16:40:03 +0100

    Upgrade @thombruce/tnt

--- components/SpoilerWarning.vue
+++ components/SpoilerWarning.vue
@@ -1,18 +1,7 @@
 <template lang='pug'>
-.alert(role='alert')
-  span.alert-context Spoiler warning!
-  span.alert-message
-    slot
+.alert.alert-error(role='alert')
+  .flex-1
+    strong.mr-2 Spoiler warning!
+    label
+      slot
 </template>
-
-<style lang='postcss'>
-.alert {
-  @apply my-5 p-2 bg-danger-800 items-center text-danger-100 leading-none flex rounded-full;
-  & span.alert-context {
-    @apply flex rounded-full bg-danger-500 uppercase px-2 py-1 text-xs font-bold mr-3;
-  }
-  & span.alert-message {
-    @apply font-semibold mr-2 text-left flex-auto;
-  }
-}
-</style>
