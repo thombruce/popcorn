7f29f48c166e50caaaeaf7f8b474678222233450
thombruce <thombruce@users.noreply.github.com>
2021-08-01 19:52:03 +0000

    deploy: def527c238e03ca8f5f5110eda6ac9c5059c159e

---
+++ _nuxt/static/1627847507/tags/marvel/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627847507",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Fmarvel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a172c9de"}}}}(null));
