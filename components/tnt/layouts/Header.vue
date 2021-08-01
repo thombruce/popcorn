def527c238e03ca8f5f5110eda6ac9c5059c159e
Thom Bruce <thom@thombruce.com>
2021-08-01 20:49:36 +0100

    Upgrade @thombruce/tnt

--- components/tnt/layouts/Header.vue
+++ components/tnt/layouts/Header.vue
@@ -2,13 +2,11 @@
 header
   span.py-24
     NuxtLink(to='/') Popcorn
-  div
+  .navbar
     nav
-      ul
-        li(v-for='(item, i) in items' :key='i')
-          NuxtLink(:to='item.to')
-            span {{ item.title }}
-    TntUIDarkmodeToggle
+      NuxtLink.btn.btn-ghost(v-for='(item, i) in items' :key='i' :to='item.to')
+        span {{ item.title }}
+    TntUIThemeToggle
 </template>
 
 <script>
@@ -42,21 +40,10 @@ export default {
 header {
   @apply flex flex-col justify-center py-5 px-4;
   & > span:first-of-type {
-    @apply block w-min m-auto text-7xl sm:text-9xl text-black dark:text-white font-poster text-chromatic-aberration;
+    @apply block w-min m-auto text-7xl sm:text-9xl text-base-content font-poster text-chromatic-aberration;
   }
   & > div {
     @apply flex justify-center;
-    & > nav {
-      & ul {
-        @apply flex;
-        & li a {
-          @apply block px-3 md:px-4;
-        }
-      }
-    }
-    & > button, & > a, & > span:not(:first-of-type) {
-      @apply block px-4;
-    }
   }
 }
 </style>
