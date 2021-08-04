7775c8bc559e40ac950db1689dba3687a3a5a37d
thombruce <thombruce@users.noreply.github.com>
2021-08-04 12:35:12 +0000

    deploy: a086cc0aeffc996005fa033cf65dc5f45fd49f67

--- _nuxt/static/1627847507/series/loki/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627847507",layout:"default",error:a,serverRendered:true,routePath:"\u002Fseries\u002Floki",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a172c9de"}}}}(null));
--- _nuxt/static/1627847507/tags/review/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627847507",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Freview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a172c9de"}}}}(null));
