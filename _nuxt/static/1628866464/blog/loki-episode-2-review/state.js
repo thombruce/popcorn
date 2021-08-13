e9680d4683ad4f2e87bf0f2672baface8d0a05db
thombruce <thombruce@users.noreply.github.com>
2021-08-13 14:54:41 +0000

    deploy: 5c605ea33c77e73729546c9703bb51270eb4b805

---
+++ _nuxt/static/1628866464/blog/loki-episode-2-review/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628866464",layout:"default",error:a,serverRendered:false,routePath:"\u002Fblog\u002Floki-episode-2-review",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c242cb0c"}}}}(null));
