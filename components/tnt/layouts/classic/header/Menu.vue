8fe95795dcdbaa4b432be828adb2fdf79ab24344
Thom Bruce <thom@thombruce.com>
2021-08-13 15:50:00 +0100

    Upgrade @thombruce/tnt

--- components/tnt/layouts/classic/header/Menu.vue
+++ components/tnt/layouts/classic/header/Menu.vue
@@ -1,12 +1,10 @@
 <template lang='pug'>
-header
-  span.py-24
-    NuxtLink(to='/') Popcorn
-  .navbar
-    nav
-      NuxtLink.btn.btn-ghost(v-for='(item, i) in items' :key='i' :to='item.to')
+.navbar
+  nav
+    template(v-for='item in items')
+      NuxtLink.btn.btn-ghost(:key='item.to' :to='item.to')
         span {{ item.title }}
-    TntUIThemeToggle
+  TntUIThemeToggle
 </template>
 
 <script>
@@ -37,13 +35,7 @@ export default {
 </script>
 
 <style lang='postcss' scoped>
-header {
-  @apply flex flex-col justify-center py-5 px-4;
-  & > span:first-of-type {
-    @apply block w-min m-auto text-7xl sm:text-9xl text-base-content font-poster text-chromatic-aberration;
-  }
-  & > div {
-    @apply flex justify-center;
-  }
+.navbar {
+  @apply flex justify-center;
 }
 </style>
---
+++ components/tnt/layouts/classic/header/Title.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+span.py-24
+  NuxtLink(to='/') {{ title }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: this.$nuxt.$options.head.title
+    }
+  }
+}
+</script>
+
+<style lang='postcss' scoped>
+span {
+  @apply block w-min m-auto text-7xl sm:text-9xl text-base-content font-poster text-chromatic-aberration;
+}
+</style>
